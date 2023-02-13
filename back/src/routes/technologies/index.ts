4cd417097916326cc6b9b221819bbe503ee8218c
lVladimirl <infinitdungeun@gmail.com>
2023-02-13 10:28:54 -0300

    feat/delete route, checks if the user and the tech exists, return errors messages properly

--- back/src/routes/technologies/index.ts
+++ back/src/routes/technologies/index.ts
@@ -2,13 +2,23 @@ import { Router } from "express";
 import { authTokenMiddleware } from "../../middlewares/authToken.middleware";
 import technologiesCreateController from "../../controllers/technologies/technologiesCreate.controller";
 import technologiesUpdateController from "../../controllers/technologies/technologiesUpdate.controller";
+import technologiesDeleteController from "../../controllers/technologies/technologiesDelete.controller";
 
 const user = Router();
 
 const technologyRouter = () => {
-  user.get("/",);
-  user.post("/register", authTokenMiddleware, technologiesCreateController)
-  user.patch("/update/:techId", authTokenMiddleware, technologiesUpdateController)
+  user.get("/");
+  user.post("/register", authTokenMiddleware, technologiesCreateController);
+  user.patch(
+    "/update/:techId",
+    authTokenMiddleware,
+    technologiesUpdateController
+  );
+  user.delete(
+    "/delete/:techId",
+    authTokenMiddleware,
+    technologiesDeleteController
+  );
 
   return user;
 };
