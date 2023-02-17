8b5a4e3a83a0d5757db1f8579e325e10dbb702ec
lVladimirl <infinitdungeun@gmail.com>
2023-02-17 14:03:27 -0300

    feat: fixed the token, login and get technologies cycle from the homepage

--- front/src/components/techModal/techModal.tsx
+++ front/src/components/techModal/techModal.tsx
@@ -9,14 +9,9 @@ import CloseIcon from "@mui/icons-material/Close";
 import { useForm } from "react-hook-form";
 import { TechSchema } from "../../schema";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { api } from "../../utils/api";
 
-export const TechModal = ({
-  open,
-  handleModal,
-  type,
-  techData,
-  setState,
-}: ModalTechsProps): ReactElement => {
+export const TechModal = ({open,handleModal,type,techData,setState,fetchTech}: ModalTechsProps): ReactElement => {
   const {
     register,
     handleSubmit,
@@ -24,134 +19,58 @@ export const TechModal = ({
   } = useForm<TechFormValues>({
     resolver: yupResolver(TechSchema),
   });
-  const onSubmit = handleSubmit((data) => {
+  
+  const onSubmit = handleSubmit(async (data) => {
     if (type === "register") {
-      setState({
-        isOpenAlert: true,
-        vertical: "top",
-        horizontal: "right",
-        ResponseType: "error",
-        error: {
-          status: 400,
-          message: "error message",
-        },
-      });
+      const formatedData = { name: data.Techname, status: data.Techstatus };
+      const response = await api.post("technologies/register", formatedData).catch((error) => error);
+      if (response.status === 201) {
+        setState({ isOpenAlert: true, vertical: "top", horizontal: "right", ResponseType: "sucess", sucess: {message: "Tecnologia Registrada"} });
+        fetchTech()
 
-      // if(axios request == error)
-      // if (response.status !== 201) {
-      //   setState({
-      //     isOpenAlert: true,
-      //     vertical: "top",
-      //     horizontal: "right",
-      //     ResponseType: "success",
-      //   });
-      // } else {
-      //  setState({
-      //   isOpenAlert: true,
-      //   vertical: "top",
-      //   horizontal: "right",
-      //   ResponseType: "error",
-      //   error: {
-      //     status: respose.status,
-      //     message:response.message,
-      //   }
-      // });
-      // }
+      } else {
+        setState({ isOpenAlert: true, vertical: "top", horizontal: "right", ResponseType: "error", error: { status: response.response.status, message: response.response.data.message} });
+
+      }
     } else {
-      setState({
-        isOpenAlert: true,
-        vertical: "top",
-        horizontal: "right",
-        ResponseType: "success",
-      });
-      // if(axios request == error)
-      // if (response.status !== 201) {
-      //   setState({
-      //     isOpenAlert: true,
-      //     vertical: "top",
-      //     horizontal: "right",
-      //     ResponseType: "success",
-      //   });
-      // } else {
-      //  setState({
-      //   isOpenAlert: true,
-      //   vertical: "top",
-      //   horizontal: "right",
-      //   ResponseType: "error",
-      //   error: {
-      //     status: respose.status,
-      //     message:response.message,
-      //   }
-      // });
-      // }
+      const formatedData = { name: data.Techname, status: data.Techstatus,};
+      const response = await api.patch(`technologies/update/${techData?.id}`, formatedData).catch((error) => error);
+
+      if (response.status === 200) {
+        setState({ isOpenAlert: true, vertical: "top", horizontal: "right", ResponseType: "sucess", sucess: { message: "Alterações Salvas" } });
+        fetchTech()
+
+      } else {
+        setState({ isOpenAlert: true, vertical: "top", horizontal: "right", ResponseType: "error", error: { status: response.response.status, message: response.response.data.message } });
+      
+      }
     }
   });
 
   return (
     <>
-      <Modal
-        open={open}
-        onClose={handleModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
+      <Modal open={open} onClose={handleModal} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
         {type === "register" ? (
           <TechForm onSubmit={onSubmit}>
             <TechFormHeader>
               <p>cadastre sua tecnologia</p>
-              <CloseIcon
-                onClick={() => handleModal({ typeOfForm: "register" })}
-              />
+              <CloseIcon onClick={() => handleModal({ typeOfForm: "register" })} />
             </TechFormHeader>
-            <Input
-              {...register("Techname")}
-              label="Nome da Tecnologia"
-              type="text"
-              placeholder="Typescript"
-              errors={errors?.Techname?.message}
-            ></Input>
-            <Input
-              {...register("Techstatus")}
-              label="Status"
-              type="text"
-              placeholder="junior"
-              errors={errors?.Techstatus?.message}
-            ></Input>
-            <Button
-              type="submit"
-              variant="default"
-              variant_hover="default_hover"
-              size="large"
-            >
+            <Input {...register("Techname")} label="Nome da Tecnologia" type="text" placeholder="Typescript" errors={errors?.Techname?.message} ></Input>
+            <Input {...register("Techstatus")} label="Status" type="text" placeholder="junior" errors={errors?.Techstatus?.message} ></Input>
+            <Button type="submit" variant="default" variant_hover="default_hover" size="large" >
               {"Cadastrar Tecnologia"}
             </Button>
           </TechForm>
         ) : (
-          <TechForm onSubmit={onSubmit} >
+          <TechForm onSubmit={onSubmit}>
             <TechFormHeader>
               <p>Editar Sua Tecnologia</p>
               <CloseIcon onClick={() => handleModal({ typeOfForm: "edit" })} />
             </TechFormHeader>
-            <Input
-              {...register("Techname")}
-              label="Nome da Tecnologia"
-              type="text"
-              placeholder={techData?.name || "" }
-              errors={errors?.Techname?.message}
-            ></Input>
-            <Input
-              {...register("Techstatus")}
-              label="Status"
-              type="text"
-              placeholder={techData?.status || "" }
-              errors={errors?.Techstatus?.message}
-            ></Input>
-            <Button
-              type="submit"
-              variant="default"
-              variant_hover="default_hover"
-              size="large"
-            >
+            <Input {...register("Techname")} label="Nome da Tecnologia" type="text" placeholder={techData?.name || ""} errors={errors?.Techname?.message} ></Input>
+            <Input {...register("Techstatus")} label="Status" type="text" placeholder={techData?.status || ""} errors={errors?.Techstatus?.message} ></Input>
+            <Button type="submit" variant="default" variant_hover="default_hover" size="large" >
               {"Editar Tecnologia"}
             </Button>
           </TechForm>
